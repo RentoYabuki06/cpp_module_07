2d2f2f5e144e4420ff7cd4697b409c4400bb19fd
rento yabuki <rento.nky.1203@gmail.com>
2025-06-07 15:26:26 +0900

    Makefileを修正し、コンパイル時にインクルードディレクトリを追加しました。また、ヘッダーファイル「whatever.hpp」にスワップ、最小値、最大値を計算するテンプレート関数を追加しました。main.cppを更新し、これらの関数を使用するようにしました。

--- ex00/includes/whatever.hpp
+++ ex00/includes/whatever.hpp
@@ -6,14 +6,30 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 18:34:51 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/06/06 22:58:08 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/06/07 15:25:46 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-#include <string>
 #include <iostream>
+
+template <typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+const T& min(const T& a, const T& b) {
+	return (b < a) ? b : a;
+}
+
+template <typename T>
+const T& max(const T& a, const T& b) {
+	return (b > a) ? b : a;
+}
 
 #endif
